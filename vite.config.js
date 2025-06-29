d3c28345e8ebfc1dbf79353c779d762323ee945f
bxkun01 <levianthan404@gmail.com>
2025-06-29 12:57:02 +0545

    shit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/walmart/' : '/',
-    plugins: [react()]
-  }
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
